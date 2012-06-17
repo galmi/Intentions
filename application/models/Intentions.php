0e9cf25135724f93338593e86203cf29b2f2dfe5
galmi <galmi.ru@gmail.com>
2012-06-17 14:40:21 +0400

    фикс

--- application/models/Intentions.php
+++ application/models/Intentions.php
@@ -7,8 +7,8 @@ class Model_Intentions extends App_Model_Base
 		//'id' => array('Required', 'Validator:Int'),
 		'description' => 'Required',
 		//'tags' => '',
-		'wants' => 'Validator:Array',
-		'done' => 'Validator:Array'
+		'wants' => 'Array',
+		'done' => 'Array'
 	);
 
     /**
